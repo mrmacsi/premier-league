1cb22cf801e7fbd346a624744359d0eebd19356d
Macit <macitsimsek12@hotmail.com>
2021-02-12 01:42:56 +0000

    add estimations

---
+++ database/migrations/2021_02_12_014143_create_estimations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstimationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estimations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('team_id');
+            $table->integer('change_to_win')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estimations');
+    }
+}
