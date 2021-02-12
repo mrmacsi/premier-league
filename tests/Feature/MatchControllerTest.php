9971db2a2cda42523bf25369b003d897ac8ab13f
Macit <macitsimsek12@hotmail.com>
2021-02-12 01:22:54 +0000

    add cleaning

---
+++ tests/Feature/MatchControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class MatchServiceTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get(route('match.index',['week'=>1]));
+        $response->assertJsonStructure([
+            'matches',
+            'stats',
+            'week'
+        ]);
+        $response->assertJson([
+            'week'=>1
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
